22bd04c8f534143b4d9d7e76478440a128b77a4d
Chao Zhang <zhangchaogoo@gmail.com>
2023-10-31 15:17:50 +0800

    Update

--- ai/TinyModel.py
+++ ai/TinyModel.py
@@ -1,43 +1,49 @@
 import torch
 import torch.nn as nn
 
+
 class TradingModel(nn.Module):
-    def __init__(self, num_products, hidden_size=2048):
+    def __init__(self, num_products, input_size, hidden_size=2048):
         super(TradingModel, self).__init__()
-        self.lstm = nn.LSTM(input_size=num_products, hidden_size=hidden_size, batch_first=True)
+        self.lstm = nn.LSTM(
+            input_size=input_size, hidden_size=hidden_size, batch_first=True
+        )
         self.fc_buy = nn.Linear(hidden_size, num_products)  # 决定是否购买
         self.fc_price = nn.Linear(hidden_size, num_products)  # 决定卖出价格
 
     def forward(self, x):
         # x: batch_size x sequence_length x num_products
         _, (hidden, _) = self.lstm(x)
-        
+
         # 只使用最后一个时间步的隐藏状态
         hidden = hidden[-1]
-        
+
         # 二分类输出：是否购买（采用sigmoid激活）
         buy_prob = torch.sigmoid(self.fc_buy(hidden))
-        
+
         # 回归输出：期望卖出价格（可以采用ReLU激活以保证价格非负）
         expected_price = torch.relu(self.fc_price(hidden))
-        
+
         return buy_prob, expected_price
 
-# # 参数
-# num_products = 100  # 商品种类数量
-# hidden_size = 128  # LSTM隐藏层大小
+
+# 参数
+num_products = 1946  # 商品种类数量
+hidden_size = 128  # LSTM隐藏层大小
 
 # # 实例化模型
-# model = TradingModel(num_products=num_products, hidden_size=hidden_size)
+model = TradingModel(
+    num_products=num_products, input_size=num_products * 4 + 3, hidden_size=hidden_size
+)
 
 # # 示例输入数据
 # # 假设我们有10天的数据，每天的数据包含100个商品的价格
-# sequence_length = 10
-# batch_size = 1
-# x_dummy = torch.randn(batch_size, sequence_length, num_products)
+sequence_length = 19
+batch_size = 1
+x_dummy = torch.randn(batch_size, sequence_length, num_products * 4 + 3)
 
 # # 模型预测
-# buy_prob, expected_price = model(x_dummy)
+buy_prob, expected_price = model(x_dummy)
 
-# print(x_dummy.shape)
-# print(buy_prob,expected_price)
+print(x_dummy.shape)
+print(buy_prob.shape, expected_price.shape)
--- ai/train.py
+++ ai/train.py
@@ -14,37 +14,100 @@ def get_codes():
     return codes
 
 
-def get_training_datas(codes):
+def get_training_datas():
     print("Start reading")
-    datas_x = dict()
-    datas_y = dict()
-    for code in codes["code"]:
-        datas_x[code] = pd.read_csv(f"train_data/_{code}_x.csv")
-        datas_y[code] = pd.read_csv(f"train_data/_{code}_y.csv")
+    x = pd.read_csv(f"train_data/100806_230918_x.csv")
+    y = pd.read_csv(f"train_data/100806_230918_y.csv")
+    # totensor
+    x = x.drop(columns=["date", "date2"])
+    y = y.drop(columns=["date", "date2", "month", "day", "weekday"])
+    y_buy_decision = y.iloc[:, ::2]
+    y_sell_price = y.iloc[:, 1::2]
+    x = torch.tensor(x.values).to(torch.float)
+    y_buy_decision = torch.tensor(y_buy_decision.values).to(torch.float)
+    y_sell_price = torch.tensor(y_sell_price.values).to(torch.float)
+    x = x.unsqueeze(1)
+    x = x.unsqueeze(1)
+    y_buy_decision = y_buy_decision.unsqueeze(1)
+    y_sell_price = y_sell_price.unsqueeze(1)
+    print(x.shape)
+    print(y_buy_decision.shape)
+    print(y_sell_price.shape)
     print("Reading done")
-    pass
+    return x, y_buy_decision, y_sell_price
+
+
+def trading_loss_function(
+    buy_decision_prob, actual_buy_decision, expected_sell_price, actual_sell_decision
+):
+    # 买入决策损失（二元交叉熵）
+    buy_decision_loss = nn.BCELoss()(buy_decision_prob, actual_buy_decision)
+
+    # 检查是否有成交的实际价格
+    sell_executed = actual_buy_decision == 1.0
+    # print(sell_executed)
+
+    sell_decision_loss = nn.MSELoss()(expected_sell_price, actual_sell_decision)
+    sell_decision_loss = torch.where(
+        sell_executed, sell_decision_loss, torch.tensor(0.0, requires_grad=True)
+    )
+    # print(buy_decision_loss, sell_decision_loss)
+    sell_decision_loss = torch.mean(sell_decision_loss)
+
+    return buy_decision_loss + sell_decision_loss
+
+    # 如果有成交，则计算盈利；如果没有成交，则为0
+    profit = torch.where(
+        sell_executed, expected_sell_price - purchase_price, torch.tensor(0.0)
+    )
+
+    # 盈利最大化损失
+    profit_loss = -torch.mean(profit)
+
+    # 执行可能性损失
+    execution_loss = torch.mean(
+        (actual_prices - expected_sell_price).pow(2) * (1 - sell_executed)
+    )
+
+    # 将买入决策损失、盈利最大化损失和执行可能性的损失结合起来
+    combined_loss = buy_decision_loss + profit_loss + execution_loss
+
+    return combined_loss
 
 
 def main():
     codes = get_codes()
-    datas = get_training_datas(codes)
-    return
+    print(codes.shape[0])
+    x, y_buy_decision, y_sell_price = get_training_datas()
+    epoch = 10
+
     device = "cuda" if torch.cuda.is_available() else "mps"
-    model = TradingModel(num_products=codes.shape[0], hidden_size=128).to(device)
+    model = TradingModel(
+        num_products=codes.shape[0], input_size=x.shape[3], hidden_size=128
+    ).to(device)
+    x = x.to(device)
+    y_buy_decision = y_buy_decision.to(device)
+    y_sell_price = y_sell_price.to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    datas = []  # todo
-    for X, y in enumerate(datas):
-        X, y = X.to(device), y.to(device)
+    for i in range(epoch):
+        for j in range(x.shape[0]):
+            input = x[j].to(device)
+            buy_decision_label = y_buy_decision[j].to(device)
+            sell_price_label = y_sell_price[j].to(device)
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+            # Compute prediction error
+            buy_prob, expected_price = model(input)
+            # print(buy_prob.shape, expected_price.shape)
 
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        print(f"loss: {loss:>7f}")
+            loss = trading_loss_function(
+                buy_prob, buy_decision_label, expected_price, sell_price_label
+            )
+
+            # Backpropagation
+            loss.backward(retain_graph=True)
+            optimizer.step()
+            optimizer.zero_grad()
+            print(f"loss: {loss:>7f}")
 
 
 if __name__ == "__main__":
